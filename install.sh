d7653c6224185d2e4aa319c03459958e72cc7fcb
piotrlechowicz <piotr.tobiasz.lechowicz@gmail.com>
2015-11-28 21:19:27 +0100

    adding files to the directory

--- install.sh
+++ install.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
 # get current directory
-location=$(pwd)
+location=$(dirname $0)
 echo $location
-
+# files which have to be added
 files_to_add=(".vimrc.vim" ".bash_bindings" ".inputrc")
-destination_files=(".vimrc" ".bash_profile" ".inputrc")
-echo ${HOME}
+#destination_files=(".vimrc" ".bash_profile" ".inputrc")
+destination_files=("a" "b" "c")
+#home=${HOME}
+home=$location
 
 # loop thorugh all files
 # get nr of elementes
 indices=${!files_to_add[*]} 
+
 for index in $indices; do
+
+	destination="$home/${destination_files[$index]}"
+	echo $destination
+
+	# check if file exists
+    if [ -f "$destination_file" ]; then
+		# append to it correct path
+		echo "exist"
+	else echo "else";
+		# create file
+		touch $destination
+	fi
+
 	echo ${files_to_add[$index]}
 	echo ${destination_files[$index]}
 done
